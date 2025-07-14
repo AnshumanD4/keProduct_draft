d3f6b761e2d923ae397d2d59e4ea0ce9954f6bbf
anshumand4 <adiwakar19@gmail.com>
2025-07-14 17:56:53 +0530

    first commit

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -101,7 +101,7 @@ const Collection = () => {
               <input className='w-3' type="checkbox" value={'Women'} onChange={toggleCategory}/> Women
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory}/> Kids
+              <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory}/> kids
             </p>
           </div>
         </div>
@@ -118,12 +118,6 @@ const Collection = () => {
             <p className='flex gap-2'>
               <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory}/> Winterwear
             </p>
-            {/* <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Cluthers'} onChange={toggleSubCategory}/> Cluthers
-            </p>
-            <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Misc'} onChange={toggleSubCategory}/> Misc
-            </p> */}
           </div>
         </div>
       </div>
